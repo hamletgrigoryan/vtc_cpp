70806580aed0e4fb14ea99f471d8edfb5726628c
Mary <maryantonyan777@gmail.com>
2016-12-16 16:58:05 +0400

    Added assert for DEBUG mode

---
+++ Mary_Antonyan/Assert/my_assert.h
@@ -0,0 +1,12 @@
+#ifndef ASSERT_H
+#define ASSERT_H
+
+#include <iostream>
+
+#ifdef DEBUG 
+#define my_assert(x) if (!(x)) { std:: cout << "error in " << __FUNCTION__ << ", line" << __LINE__ <<" , condition: " << #x << std::endl; }
+#else
+#define my_assert(x) if(x){}
+#endif
+
+#endif
