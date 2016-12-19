4b214ea55259edcc9341aa2376b8d1b1a6044a84
koryun <koryundemirkhanyan@gmail.com>
2016-12-19 18:03:02 +0400

    Added assert. x

---
+++ KoryunDemirkhanyan/assert.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+//Debug - assert on  RELEASE - assert off
+
+#ifdef DEBUG
+#define my_assert(x) if(!(x)) { std::cout<<"error in" << _FUNCTION_<<",line"<< _LINE_ << x << std::endl; }
+#else
+#define my_assert(x)
+#endif
+
+int main()
+{
+        int x=-2;
+        my_assert(x>0)
+	return 0;
+}
+
