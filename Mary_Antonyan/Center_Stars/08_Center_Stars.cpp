69f095ea7127919c1f486168dd506409361797fb
Mary Antonyan <maryantonyan777@gmail.com>
2017-01-04 19:26:06 +0400

    Added the center stars' program

---
+++ Mary_Antonyan/Center_Stars/08_Center_Stars.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	unsigned int n;
+	std::cout << "Enter n: ";
+	std::cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n -  i; j++)
+			std::cout << ' ';
+
+		for (int j = 0; j < 2 * i + 1; j++)
+			std::cout << '*';
+
+		std::cout << std::endl;
+	}
+
+	system("pause");
+	return 0;
+}
