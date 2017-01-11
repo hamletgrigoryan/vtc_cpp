66ba9b1803384672d2ecf77f06adc3e4b508cd3b
Koryun <koryundemirkhanyan@gmail.com>
2017-01-12 01:08:59 +0400

    program chess

--- KoryunDemirkhanyan/Chess/chess_new.cpp
+++ KoryunDemirkhanyan/Chess/chess_new.cpp
@@ -1,54 +1,25 @@
 #include<iostream>
 int main ()
 {
-    for(int x=0;x<4;++x)
-    {
-        for(int x=0;x<8;++x)
-        {
-            std::cout<<" ___";
-        }
-        std::cout<<std::endl;
-        for(int x=0;x<4;++x)
-        { 
-             std::cout<<"|XXX|   ";
-        }
-         std::cout<<"|"<<std::endl;
-        for(int x=0;x<4;++x)
-        { 
-            std::cout<<"|XXX|   ";
-        }
-        std::cout<<"|"<<std::endl;
-        for(int x=0;x<4;++x)
-        { 
-            std::cout<<"|XXX|   ";
-        }
-        std::cout<<"|"<<std::endl;
-        for(int x=0;x<8;++x)
-        {
-            std::cout<<" ___";
-        }
-        std::cout<<std::endl;
-        for(int x=0;x<4;++x)
-        { 
-            std::cout<<"|   |XXX";
-        }
-        std::cout<<"|"<<std::endl;
-        for(int x=0;x<4;++x)
-        { 
-            std::cout<<"|   |XXX";
-        }
-        std::cout<<"|"<<std::endl;
-        for(int x=0;x<4;++x)
-        { 
-            std::cout<<"|   |XXX";
-        }
-        std::cout<<"|"<<std::endl;
-    }    
-    for(int x=0;x<8;++x)
-        {
-            std::cout<<" ___";
-        }
-    std::cout<<std::endl;
-    
+const int board_size = 8;
+const char white=' ';
+const char black='*';
+const int  size = 3;
+const int s = size + 1;
+
+for(int i = 0; i <= board_size * s; ++i) {
+	for(int j = 0; j <= board_size * s; ++j) {
+		if (i % s == 0) {
+			std::cout << '_';
+			} else if (j % s ==0) {
+			    std::cout << '|';
+                        } else if ((i / s + j / s) % 2 == 0) {
+                            std::cout << white; 
+	                } else {
+                            std::cout << black;
+				}
+			}
+			std::cout << std::endl; 
+	}
 return 0;
 }
