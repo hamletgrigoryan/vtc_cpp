c3b64b4b19624188f2bd9b69a63900af9151787d
Koryun <koryundemirkhanyan@gmail.com>
2017-01-19 02:11:53 +0400

    add sort

---
+++ KoryunDemirkhanyan/Array/bubble_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+
+int main()
+{
+    int a[100] = {};
+    int tmp;    
+    srand(time(0));
+    for (int i = 0; i < 100; i++)
+    {
+        a[i] = rand() % 101;
+        std::cout << a[i] << ' ';
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < 100; ++i) 
+    {
+        for(int j = i+1; j < 100; ++j) {
+            if (a[i] > a[j]) {
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+    }
+    for (int i = 0; i < 100; ++i) 
+    {
+        std::cout << a[i] << ' ';
+    }            
+    std::cout << std::endl;
+    return 0;
+}
---
+++ KoryunDemirkhanyan/Array/selection_sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+
+int main()
+{
+    int a[10] = {};
+    int tmp;
+    srand(time(0));
+    for (int i = 0; i < 10; i++)
+    {
+        a[i] = rand() % 11;
+        std::cout << a[i] << ' ';
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < 10-1; ++i)
+    {
+        for (int j = i+1; j < 10; ++j)
+        {
+            if (a[i] > a[j])
+            {
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+    }
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << a[i] << ' ';
+    }
+    std::cout <<std::endl;
+
+    return 0;
+}
