b6b3a9d288b33f8e836a3ab8bee5a2d61583d1e8
Koryun <koryundemirkhanyan@gmail.com>
2017-02-13 01:20:16 +0400

    add file.h

--- KoryunDemirkhanyan/Bsearch/b_search.cpp
+++ KoryunDemirkhanyan/Bsearch/b_search.cpp
@@ -1,26 +1,6 @@
 #include<iostream>
 #include<cstdlib>
-
-void input_array (int *array, int size)
-{
-    srand (time(0));
-    for(int i = 0; i < size; i++)
-    {
-        array[i] = 1 + rand() % 100;
-        std::cout << array[i] << ' ';
-    }
-    std::cout << std::endl;
-}
-
-void output_array (int *array, int size)
-{
-    for(int i = 0; i < size; i++)
-    {
-        std::cout << array[i] << ' ';
-    }
-    std::cout << std::endl;
-}
-
+#include"ioa.h"
 
 int compare (const void *a, const void *b)
 {
