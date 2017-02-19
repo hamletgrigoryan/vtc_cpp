30514abbf928156d512f5684b5fa72b7f29693c5
SERJ <serjarturich@mail.ru>
2017-02-19 20:52:25 +0400

    steel liq@ harcer

---
+++ serjjan/erankyuni/VERJ.cpp
@@ -0,0 +1,44 @@
+#include "NKARAGRUTYUN.cpp"
+#include <iostream>
+
+int main()
+{
+	int x1, x2, x3, y1, y2, y3;
+	
+	std::cout << "x1 = ";
+	std::cin >> x1;
+
+	std::cout <<"x2 = ";
+	std::cin >> x2;
+
+	std::cout << "x3 = ";
+	std::cin >> x3;
+
+	std::cout << "y1 = ";
+	std::cin >> y1;
+	
+	std::cout << "y2 = ";
+	std::cin >> y2;
+
+	std::cout << "y3 = ";
+	std::cin >> y3;
+
+	int x, y ;
+
+	std::cout << "x = " ;
+	std::cin >> x;
+
+	std::cout << "y = ";
+	std::cin >> y ;
+
+	if(object.contain(x , y))
+	{
+		std::cout << "nersne" ;
+	}else
+	{
+		std::cout << "dursne ";
+	}
+
+	return 0;
+
+} 
