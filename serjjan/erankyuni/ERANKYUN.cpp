d1d551a7dc794ef44fb72641199cc655556fc2e6
SERJ <serjarturich@mail.ru>
2017-02-19 20:41:30 +0400

    liq@ harcer

---
+++ serjjan/erankyuni/ERANKYUN.cpp
@@ -0,0 +1,15 @@
+#include "KET.hpp"
+
+class tringle
+{
+private:
+point a;
+point b;
+point c;
+public:
+void point::set (point, point, point);
+tringle (point, point, point);
+int makeres();
+bool stugum (int x, int y);
+
+};
