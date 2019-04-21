b60aebef531b3f8d374b242f4bfcd3b9fe586ee5
SerjArturich <serjarturich@mail.ru>
2019-04-21 21:46:23 +0400

    Update ERANKYUN.cpp

--- serjjan/erankyuni/ERANKYUN.cpp
+++ serjjan/erankyuni/ERANKYUN.cpp
@@ -1,15 +1,31 @@
-#include "KET.hpp"
+#include "triangle.hpp"
+#include <cmath>
 
-class tringle
+triangle::
+triangle(const point& a, const point& b, const point& c)
+    : m_a(a)
+    , m_b(b)
+    , m_c(c)
+{}
+
+double triangle::
+area() const
 {
-private:
-point a;
-point b;
-point c;
-public:
-void point::set (point, point, point);
-tringle (point, point, point);
-int makeres();
-bool stugum (int x, int y);
+    double a = m_a.distance(m_b);
+    double b = m_a.distance(m_c);
+    double c = m_b.distance(m_c);
+    double p = (a + b + c) / 2;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
 
-};
+bool triangle::
+contains(const point& p) const
+{
+    double a = (m_a.x() - p.x()) * (m_b.y() - m_a.y()) - (m_b.x() - m_a.x()) * (m_a.y() - p.y());
+    double b = (m_b.x() - p.x()) * (m_c.y() - m_b.y()) - (m_c.x() - m_b.x()) * (m_b.y() - p.y());
+    double c = (m_c.x() - p.x()) * (m_a.y() - m_c.y()) - (m_a.x() - m_c.x()) * (m_c.y() - p.y());
+    return (a <= 0 && b <= 0 && c <= 0) || (a >= 0 && b >= 0 && c >= 0);
+}
+
+
+
